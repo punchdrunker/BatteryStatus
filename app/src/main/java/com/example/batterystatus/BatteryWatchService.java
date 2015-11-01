d37e0cd51deccaec02a753a80449337268635d0b
punchdrunker <nanao.hhk@gmail.com>
2015-11-01 16:01:27 +0900

    TAGをクラス名にした nullチェックをした

--- app/src/main/java/com/example/batterystatus/BatteryWatchService.java
+++ app/src/main/java/com/example/batterystatus/BatteryWatchService.java
@@ -11,7 +11,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class BatteryWatchService extends Service {
-    static String TAG = "TAG";
+    static String TAG = "BatteryWatchService";
     private Timer timer;
 
     public BatteryWatchService() {
@@ -22,6 +22,7 @@ public class BatteryWatchService extends Service {
         Log.d(TAG, "onBind");
         return null;
     }
+
     @Override public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
@@ -34,15 +35,15 @@ public class BatteryWatchService extends Service {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Log.i(TAG, "WORKING");
-
                 IntentFilter intentfilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-                Intent batteryStatus = registerReceiver(null, intentfilter );
+                Intent batteryStatus = registerReceiver(null, intentfilter);
 
-                int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-                Log.d("Battery", "Level: " + String.valueOf(batteryLevel));
+                if (batteryStatus!=null) {
+                    int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                    Log.d(TAG, "Level: " + String.valueOf(batteryLevel));
+                }
             }
-        }, 0, 3000);
+        }, 0, 5000);
 
         return START_STICKY;
     }
