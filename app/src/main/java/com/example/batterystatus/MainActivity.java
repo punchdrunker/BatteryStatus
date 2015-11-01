61b50d1a85ca6d245395013d32094c47a6de24c9
punchdrunker <nanao.hhk@gmail.com>
2015-11-01 17:39:21 +0900

    課題の作成

--- app/src/main/java/com/example/batterystatus/MainActivity.java
+++ app/src/main/java/com/example/batterystatus/MainActivity.java
@@ -21,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(new Intent(getApplicationContext(), BatteryWatchService.class));
+                // TODO: 2.BatteryWatchServiceを起動しよう
             }
         });
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopService(new Intent(getApplicationContext(), BatteryWatchService.class));
+                // TODO: 3.BatteryWatchServiceを停止しよう
             }
         });
+
+        // TODO: 1.バッテリー残量を取得してLog出力してみよう
     }
 
     @Override
